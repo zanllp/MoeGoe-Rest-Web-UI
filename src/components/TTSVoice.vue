e7bcf69d1fb59fe77790ade0f7c8e67201c64a4c
zanllp <qc@zanllp.cn>
2023-01-23 04:11:53 +0800

    优化语言标记

--- src/components/TTSVoice.vue
+++ src/components/TTSVoice.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { onMounted, reactive, ref, toRaw } from 'vue'
 import { generateTTSVoice, TTSVoiceGenerateParams } from '@/api/tts'
-import { SearchSelect } from 'vue3-ts-util'
+import { SearchSelect, ok } from 'vue3-ts-util'
 import { InboxOutlined } from '@ant-design/icons-vue'; import { message, UploadProps } from 'ant-design-vue'
+
+
 const fileList = ref<UploadProps['fileList']>([])
 const speakers = ref<string[]>([])
 const formRef = ref<{ validate (): Promise<void> }>()
+const textInputRef = ref<any>()
 const form = ref<TTSVoiceGenerateParams>({
   model_path: '',
   conf_text: '',
@@ -39,7 +42,9 @@ const submit = async () => {
 const beforeUpload = async (file: File) => {
   try {
     const txt = await file.text()
-    speakers.value = JSON.parse(txt).speakers ?? []
+    const { speakers: speakersIn } = JSON.parse(txt)
+    ok(speakersIn instanceof Array)
+    speakers.value = speakersIn
     form.value.conf_text = txt
   } catch (error) {
     message.error('不是合法的json配置文本')
@@ -47,15 +52,48 @@ const beforeUpload = async (file: File) => {
 
   return false
 }
+
+const markSelectedText = (lang: 'ZH' | 'JA' | 'EN') => {
+  const el = textInputRef.value.resizableTextArea.textArea as HTMLInputElement
+  const { selectionStart: start, selectionEnd: end } = el
+  if (!(typeof start === 'number' &&  typeof end === 'number') || start === end) {
+    message.error('需要选中分区')
+    return
+  }
+  const tag = `[${lang}]`
+  const textRaw = form.value.text
+  const res = textRaw.substring(0, start) + tag + textRaw.substring(start, end) + tag + textRaw.substring(end)
+  form.value.text = res
+}
+
+const clearTextMark = () => {
+  form.value.text = form.value.text.replace(/\[((EN)|(ZH)|(JA))\]/g,'')
+}
 </script>
 
 <template>
   <a-select v-if="false" />
   <a-form class="form" ref="formRef" :model="form">
     <a-form-item label="输入文本" required name="text">
-      <a-textarea auto-size v-model:value="form.text">
+      <a-textarea auto-size v-model:value="form.text" ref="textInputRef">
 
       </a-textarea>
+      <div class="mark-as">
+        标记为
+        <a-button size="small" @click="markSelectedText('ZH')">
+          中
+        </a-button>
+        <a-button size="small" @click="markSelectedText('JA')">
+          日
+        </a-button>
+        <a-button size="small" @click="markSelectedText('EN')">
+          英
+        </a-button>
+        <div class="flex-placeholder"></div>
+        <a-button size="small" @click="clearTextMark">
+          清理所有标记
+        </a-button>
+      </div>
     </a-form-item>
     <a-form-item label="模型地址" required name="model_path">
       <a-input v-model:value="form.model_path"></a-input>
@@ -82,25 +120,37 @@ const beforeUpload = async (file: File) => {
         提交
       </a-button>
     </a-form-item>
+    <audio v-if="mp3" controls :src="mp3">
+    </audio>
   </a-form>
-  <audio v-if="mp3" controls :src="mp3">
-  </audio>
 </template>
 
 <style scoped lang="scss">
+.flex-placeholder {
+  flex: 1;
+}
 .form {
   width: 100%;
   padding: 16px;
-}
 
-.json-preview {
-  max-height: 256px;
-  overflow: auto;
-  font-family: Consolas, Menlo, monospace;
-  font-size: 12px;
-  background-color: rgba(17, 206, 153, 0.05);
-  padding: 8px;
-  border-radius: 4px;
-  color: black;
+  .json-preview {
+    max-height: 256px;
+    overflow: auto;
+    font-family: Consolas, Menlo, monospace;
+    font-size: 12px;
+    background-color: rgba(17, 206, 153, 0.05);
+    padding: 8px;
+    border-radius: 4px;
+    color: black;
+  }
+
+  .mark-as {
+    margin-top: 4px;
+    display: flex;
+    align-items: center;
+    & > * {
+      margin-right: 4px;
+    }
+  }
 }
 </style>
